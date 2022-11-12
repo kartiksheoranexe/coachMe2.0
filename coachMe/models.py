75ba174e7ee0c5378610a62d8534c9562e22cb63
Kartik Sheoran <sheoran.kartik009@gmail.com>
2022-11-12 19:55:11 +0530

    user and address model added

--- coachMe/models.py
+++ coachMe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+class User(AbstractUser):
+    dob = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+class Àddress():
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address_line_one = models.CharField(max_length=254)
+    address_line_two = models.CharField(max_length=254)
+    long = models.DecimalField(max_digits=8, decimal_places=3)
+    lat = models.DecimalField(max_digits=8, decimal_places=3)
--- settings.py
+++ settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
+#from django.contrib.auth.models import AbstractUser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coachMe',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'coachMe.User' 
+
+
